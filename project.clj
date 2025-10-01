ca94a9259f3cd669a1078c277607e303c6be1f26
jj <keistaslogairkosulys@gmail.com>
2025-10-02 01:46:35 +0300

    copy github workflows instead of rendering

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject okapi/lein-template "1.0.0"
+(defproject okapi/lein-template "1.0.2"
 
   :description "A simple leiningen template "
   :url "https://github.com/ruroru/okapi"
   :license {:name "EPL-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[hato "1.0.0"]
+                 [org.clojars.jj/majavat "1.10.0"]
                  [org.clojure/data.json "2.5.1"]]
 
   :profiles {:test {:dependencies [[babashka/fs "0.5.27"]]}}
